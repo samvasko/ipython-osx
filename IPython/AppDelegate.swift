faed7da0d562075d8d33c01f1b53ea2bd12c0cf8
Samuel Vasko <bliker@Samuels-MacBook-Pro.local>
2014-12-17 14:32:00 +0100

    Initial Commit

---
+++ IPython/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  IPython
+//
+//  Created by Samuel Vasko on 17/12/14.
+//  Copyright (c) 2014 Samuel Vasko. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
