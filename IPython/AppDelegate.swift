9ef92e485c8a8596629e86b8de254e84b0797b4f
Samuel Vasko <samvasko@gmail.com>
2014-12-19 14:42:56 +0100

    First commit, basic structure

--- IPython/AppDelegate.swift
+++ IPython/AppDelegate.swift
@@ -11,14 +11,45 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
+    var shell: NSTask?
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        var shell = NSTask()
+        
+        shell.launchPath = "/usr/local/bin/ipython"
+        shell.arguments = ["notebook", "--no-browser"]
+        
+        let pipe = NSPipe()
+        shell.standardOutput = pipe
+
+        pipe.fileHandleForReading.waitForDataInBackgroundAndNotify()
+            
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.addObserver(self, selector: "receivedData:", name: NSFileHandleDataAvailableNotification, object: nil)
+        
+        // You can also set a function to fire after the task terminates
+        shell.terminationHandler = {task -> Void in
+            // Handle the task ending here
+        }
+        
+//        shell.launch()
+        
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
+        println("closing")
+    }
+    
+    
+    // Take care of printing output of ipyton command
+    func receivedData(notif : NSNotification) {
+        let fh:NSFileHandle = notif.object as NSFileHandle
+        let data = fh.availableData
+        if data.length > 1 {
+            fh.waitForDataInBackgroundAndNotify()
+            let string = NSString(data: data, encoding: NSASCIIStringEncoding)
+            println(string!)
+        }
     }
 
 
--- IPython/Document.swift
+++ IPython/Document.swift
@@ -7,9 +7,12 @@
 //
 
 import Cocoa
+import WebKit
 
 class Document: NSDocument {
 
+    @IBOutlet var webView: WebView!
+    
     override init() {
         super.init()
         // Add your subclass-specific initialization here.
@@ -17,23 +20,23 @@ class Document: NSDocument {
 
     override func windowControllerDidLoadNib(aController: NSWindowController) {
         super.windowControllerDidLoadNib(aController)
+        let requesturl = NSURL(string: "http://localhost:8888/notebooks/Code/Python/Untitled1.ipynb")
+        let request = NSURLRequest(URL: requesturl!)
+        webView.mainFrame.loadRequest(request)
         // Add any code here that needs to be executed once the windowController has loaded the document's window.
     }
 
     override class func autosavesInPlace() -> Bool {
-        return true
+        return false
     }
 
     override var windowNibName: String? {
-        // Returns the nib file name of the document
-        // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this property and override -makeWindowControllers instead.
         return "Document"
     }
 
     override func dataOfType(typeName: String, error outError: NSErrorPointer) -> NSData? {
-        // Insert code here to write your document to data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning nil.
-        // You can also choose to override fileWrapperOfType:error:, writeToURL:ofType:error:, or writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-        outError.memory = NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+        //
+//        outError.memory = NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
         return nil
     }
 
@@ -41,8 +44,8 @@ class Document: NSDocument {
         // Insert code here to read your document from the given data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning false.
         // You can also choose to override readFromFileWrapper:ofType:error: or readFromURL:ofType:error: instead.
         // If you override either of these, you should also override -isEntireFileLoaded to return NO if the contents are lazily loaded.
-        outError.memory = NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
-        return false
+//        outError.memory = NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+        return true
     }
 
 
